1232a6fe01cf40eaae26c83f9350682658ecd357
ALBERTO JOSE MARRERO AZUAJE <ALBERTO.MARRERO@correoaiep.cl>
2023-12-03 20:37:15 -0300

    select "usr_nombres"

--- routes/usuario.routes.ts
+++ routes/usuario.routes.ts
@@ -95,7 +95,7 @@ usuarioRoutes.get('/', async (req: Request, res: Response) => {
     try {
         const client = await pool.connect();
         const usuarios = await client.query(
-            `SELECT * FROM "USUARIO";`
+            `SELECT "USR_NOMBRES" FROM "USUARIO";`
         );
         console.log("Consulta Select usuario Ok:")
 
