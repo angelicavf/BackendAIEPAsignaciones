7dad610f56f727d4aecc91058cbb6c8135ed9892
angelicavf <angelicamvf@gmail.com>
2023-12-04 21:32:12 -0300

    modifica select act

--- routes/actividad.routes.ts
+++ routes/actividad.routes.ts
@@ -47,17 +47,19 @@ actividadRoutes.get('/', async (req: Request, res: Response) => {
     try {
         const client = await pool.connect();
         const actividad = await client.query(
-            `SELECT coalesce(U2."USR_ID", p."USR_ID") as "USR_ID", coalesce(U2."USR_NOMBRES", p."USR_NOMBRES") as "USR_NOMBRES", p."ACT_NOMBRE",p.HORAS
-            FROM "USUARIO" U2
-            LEFT JOIN (SELECT U."USR_ID", "AGE_FECHA",U."USR_NOMBRES", A."ACT_NOMBRE", array_to_json(array_agg(H.*)) as HORAS
-                  FROM "AGENDA"
-                           LEFT JOIN public."ACTIVIDAD" A on "AGENDA"."AGE_ID" = A."ACT_AGE_ID"
-                           LEFT JOIN public."USUARIO" U on U."USR_ID" = "AGENDA"."AGE_USR_ID"
-                           LEFT JOIN public."AGENDA_HORA" AH on "AGENDA"."AGE_ID" = AH."AH_AGE_ID"
-                           LEFT JOIN public."HORA" H on AH."AH_HOR_ID" = H."HOR_ID"
-                  WHERE "AGE_FECHA" = '2023-12-07'
-                  GROUP BY U."USR_ID", U."USR_NOMBRES", A."ACT_NOMBRE", "AGE_FECHA") as p on U2."USR_ID" = p."USR_ID"
-            order by U2."USR_ID"`
+            `SELECT coalesce(U2."USR_ID", p."USR_ID") as "USR_ID", coalesce(U2."USR_NOMBRES", p."USR_NOMBRES") as "USR_NOMBRES",
+            p."ACT_NOMBRE", P."ACT_ESTADO",
+            p.HORAS
+     FROM "USUARIO" U2
+     LEFT JOIN (SELECT U."USR_ID", "AGE_FECHA",U."USR_NOMBRES", A."ACT_ESTADO", A."ACT_NOMBRE", json_agg(H."HOR_VALOR") as HORAS
+           FROM "AGENDA"
+                    LEFT JOIN public."ACTIVIDAD" A on "AGENDA"."AGE_ID" = A."ACT_AGE_ID"
+                    LEFT JOIN public."USUARIO" U on U."USR_ID" = "AGENDA"."AGE_USR_ID"
+                    LEFT JOIN public."AGENDA_HORA" AH on "AGENDA"."AGE_ID" = AH."AH_AGE_ID"
+                    LEFT JOIN public."HORA" H on AH."AH_HOR_ID" = H."HOR_ID"
+           WHERE "AGE_FECHA" = '2023-12-07'
+           GROUP BY U."USR_ID", U."USR_NOMBRES", A."ACT_NOMBRE", "AGE_FECHA", A."ACT_ESTADO") as p on U2."USR_ID" = p."USR_ID"
+     order by U2."USR_ID"`
         );
         console.log("Consulta Select Realizada:")
         client.release();
